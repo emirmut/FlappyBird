2c9cb0f93fdbedf0f0cad9b17b26bcaf26737e21
Emir Mut <emirmut1903@gmail.com>
2025-03-03 19:07:07 +0300

    Finalized the project.

--- Assets/Scripts/PipeMovement.cs
+++ Assets/Scripts/PipeMovement.cs
@@ -7,7 +7,7 @@ public class PipeMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Destroy(gameObject, 7);
+        Destroy(gameObject, 10);
     }
 
     // Update is called once per frame
